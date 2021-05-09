5824825e7ce2c666c6aa31518848efba2f35500e
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-09 17:36:36 -0300

    feat: Create hoc with SSR validation for guests

--- reactjs/05-nextjs-authentication/pages/index.tsx
+++ reactjs/05-nextjs-authentication/pages/index.tsx
@@ -1,6 +1,8 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -29,3 +31,9 @@ export default function Home() {
     </form>
   )
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
