63c70e9fbc6f60586366b947fc4d00b960c6e799
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-15 22:21:32 -0300

    feat: Add memo to ProductItem for less re-renders

--- reactjs/06-reactjs-performance/components/ProductItem.tsx
+++ reactjs/06-reactjs-performance/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react"
+
 type Product = {
   id: number;
   price: number;
@@ -9,10 +11,14 @@ interface ProductItemProps {
 }
 
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   )
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
