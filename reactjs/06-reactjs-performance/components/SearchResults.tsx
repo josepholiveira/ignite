04bcc89c67866c60b54a8254533bc6b515d87c65
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-15 22:41:48 -0300

    feat: Add memoized total price calculation

--- reactjs/06-reactjs-performance/components/SearchResults.tsx
+++ reactjs/06-reactjs-performance/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import { ProductItem } from "./ProductItem"
 
 type Product = {
@@ -11,8 +12,15 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price
+    }, 0)
+  }, []);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map(product => {
         return (
           <ProductItem product={product} />
