d5e704dd4539edd3637971cab66586380d998aa6
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-16 10:20:45 -0300

    chore: Add react-virtualized for better list performance

--- reactjs/06-reactjs-performance/components/SearchResults.tsx
+++ reactjs/06-reactjs-performance/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from 'react-virtualized'
 import { ProductItem } from "./ProductItem"
 
 type Product = {
@@ -14,19 +15,28 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, totalPrice, onAddToWishlist }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    )
+  } 
 
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishlist={onAddToWishlist}
-          />
-        )
-      })}
+      <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
