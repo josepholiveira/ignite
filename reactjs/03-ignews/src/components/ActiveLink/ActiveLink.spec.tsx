28cfce7ce3240d4cd0578df30c5b22f7deea6736
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-19 11:37:08 -0300

    test: Refactor ActiveLink tests

--- reactjs/03-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
+++ reactjs/03-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { ActiveLink } from '.'
 
 jest.mock('next/router', () => {
@@ -13,23 +13,23 @@ jest.mock('next/router', () => {
 
 describe('ActiveLink component', () => {
   it('renders correctly', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
       </ActiveLink> 
     )
   
-    expect(getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('Home')).toBeInTheDocument()
   })
   
   it('adds active class if the link is the same as the page', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
       </ActiveLink> 
     )
   
-    expect(getByText('Home')).toHaveClass('active')
+    expect(screen.getByText('Home')).toHaveClass('active')
   })
 })
 
