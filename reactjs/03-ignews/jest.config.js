13419af68294beb5799b5f6ffe99f5b2014f0c39
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-19 11:12:16 -0300

    chore: Initial testing structure

---
+++ reactjs/03-ignews/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts"
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  }
+}
