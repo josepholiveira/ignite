ebf399c6789c352dc8a35808920f9476d8d1dad2
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-19 11:29:20 -0300

    test: Add active link tests

--- reactjs/03-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
+++ reactjs/03-ignews/src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,12 +1,35 @@
 import { render } from '@testing-library/react'
 import { ActiveLink } from '.'
 
-test('active link renders correctly', () => {
-  const { debug } = render(
-    <ActiveLink href="/" activeClassName="active">
-      <a>Home</a>
-    </ActiveLink> 
-  )
-
-  debug()
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/'
+      }
+    }
+  }
 })
+
+describe('ActiveLink component', () => {
+  it('renders correctly', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink> 
+    )
+  
+    expect(getByText('Home')).toBeInTheDocument()
+  })
+  
+  it('adds active class if the link is the same as the page', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink> 
+    )
+  
+    expect(getByText('Home')).toHaveClass('active')
+  })
+})
+
