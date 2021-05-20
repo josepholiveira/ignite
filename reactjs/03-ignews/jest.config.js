725ffafc44b788c4c19142eecfc65682bf5b92c6
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-05-20 11:04:07 -0300

    test: Add test coverage

--- reactjs/03-ignews/jest.config.js
+++ reactjs/03-ignews/jest.config.js
@@ -8,5 +8,13 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(scss|css|sass)$": "identity-obj-proxy"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx"
+  ],
+  coverageReporters: ["lcov", "json"]
 }
